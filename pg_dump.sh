9c6718a525b705b21c24577d621ff21377bdd8da
Fausto Ojeda Moreno <59848825+FaustoOjedaMoreno@users.noreply.github.com>
2023-12-10 09:39:19 -0700

    Update pg_dump.sh
    Comment

--- pg_dump.sh
+++ pg_dump.sh
@@ -1,2 +1,4 @@
+#backup full to databases instance
+for datname in $(psql -c "SELECT datname FROM pg_database WHERE datname NOT IN ('template0','template1','postgres') ORDER BY pg_database_size(datname) ASC;" -t); do /usr/local/pgsql/bin/pg_dump -C "${datname}" | gzip -c9 > ${datname}_full.sql.gz; done
 #Generate a backup with insert statements of a table
 pg_dump --dbname=datname --table=tablename --encoding=encodingname --data-only --column-inserts > file.sql
